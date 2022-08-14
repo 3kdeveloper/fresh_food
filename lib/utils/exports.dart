fc4f14496e9372292274ef9e432e0966b13ca929
3kdeveloper <kamranktk807@gmail.com>
2022-08-14 16:01:39 +0500

    organizing code structure and creating splash screen

---
+++ lib/utils/exports.dart
@@ -0,0 +1,11 @@
+export 'package:flutter/material.dart';
+
+// Packages
+export 'package:introduction_screen/introduction_screen.dart';
+
+// View
+export 'package:fresh_food/views/on_boarding/on_boarding_screen.dart';
+
+//--Components
+// Models
+// Controllers
